d7330625588f762f89a06b1a517e28e24342bee3
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-06-25 19:37:14 -0300

    Add files via upload

---
+++ TringuloCurso/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace TringuloClass
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double calcularArea(double A, double B, double C)
+        {
+            double p = (A + B + C) / 2.0;
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return area;
+        }
+    }
+}
---
+++ TringuloCurso/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TringuloClass;
+
+namespace TringuloCurso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo A = new Triangulo();
+            Console.WriteLine("Entre com as medidas do triângulo A:");
+            A.A = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            A.B = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            A.C = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            Triangulo B = new Triangulo();
+            Console.WriteLine("Entre com as medidas do triângulo B:");
+            B.A = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            B.B = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+            B.C = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+            double AreaA = A.calcularArea(A.A, A.B, A.C);
+            double AreaB = B.calcularArea(B.A, B.B, B.C);
+
+            Console.WriteLine("Area A: " + AreaA.ToString("F4", System.Globalization.CultureInfo.InvariantCulture));
+            Console.WriteLine("Area B: " + AreaB.ToString("F4", System.Globalization.CultureInfo.InvariantCulture));
+
+            if(AreaA > AreaB)
+            {
+                Console.WriteLine("Maior Area: A");
+            } else if (AreaB > AreaA) 
+            {
+                Console.WriteLine("Maior Area: B");
+            }
+            
+        }
+    }
+}
